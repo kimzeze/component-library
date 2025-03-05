c2f3e49ef4c55d9c42a4bdda09bd5bbac615d1a9
kimzeze <fridaynight@kakao.com>
2025-03-05 22:47:45 +0900

    asset: 앱티핏 로고 및 아이콘 에셋 추가
    프로젝트에 새로운 SVG 아이콘 및 로고 에셋을 도입합니다:
    - eye-off.svg: 비밀번호 숨김 상태 아이콘
    - eye-on.svg: 비밀번호 표시 상태 아이콘
    - logo-aptifit.svg: 앱티핏 공식 로고

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+/* 비밀번호 표시 아이콘 */
+import EyeOff from "./eye-off.svg";
+import EyeOn from "./eye-on.svg";
+/* 로고 */
+import LogoAptifit from "./logo-aptifit.svg";
+
+export { EyeOn, EyeOff, LogoAptifit };
