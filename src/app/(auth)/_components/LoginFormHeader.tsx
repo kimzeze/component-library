cda2809b80bcc00d593ad59f507b779f0d4f4b77
kimzeze <fridaynight@kakao.com>
2025-03-13 16:30:38 +0900

    feat: 로그인 폼 푸터, 헤더 컴포넌트 추가
    사용자 인증을 위한 새로운 AuthFooter 컴포넌트를 구현하였습니다. 이 컴포넌트는 링크와 저작권 정보를 포함하며, 기본 링크로 카카오톡 상담 및 고객센터 안내를 제공합니다.

---
+++ src/app/(auth)/_components/LoginFormHeader.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function AuthHeader() {
+  return (
+    <header className="border text-center">
+      <h1 className="text-md font-medium">로그인 메인 제목</h1>
+      <p className="text-md">로그인 설명은 여기에 적습니다.</p>
+    </header>
+  );
+}
