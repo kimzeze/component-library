4ad6b35a2c3164acec2ba4e993f9c36f8458727b
kimzeze <fridaynight@kakao.com>
2025-03-13 16:33:28 +0900

    chore: 로고 아이콘 삭제 및 관련 코드 정리
    사용되지 않는 로고 아이콘 파일(logo-aptifit.svg)을 삭제하고, 아이콘 내보내기에서 해당 아이콘을 제거하여 코드 정리를 진행하였습니다.

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,5 @@
 /* 비밀번호 표시 아이콘 */
 import EyeOff from "./eye-off.svg";
 import EyeOn from "./eye-on.svg";
-/* 로고 */
-import LogoAptifit from "./logo-aptifit.svg";
 
-export { EyeOn, EyeOff, LogoAptifit };
+export { EyeOff, EyeOn };
