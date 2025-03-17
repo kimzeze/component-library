37ca24ef29aa819cf764a83d96214ec183d23a5c
kimzeze <fridaynight@kakao.com>
2025-03-17 17:00:54 +0900

    design: 로그인 폼 제목 및 설명 변경
    로그인 폼의 제목을 '로그인 폼 제목'으로, 설명을 '로그인 폼 설명'으로 수정하여 사용자에게 보다 명확한 정보를 제공하도록 개선하였습니다.

--- src/app/(auth)/_components/LoginFormHeader.tsx
+++ src/app/(auth)/_components/LoginFormHeader.tsx
@@ -1,8 +1,8 @@
 export default function LoginFormHeader() {
   return (
     <header className="text-center">
-      <h1 className="text-md font-medium">로그인 메인 제목</h1>
-      <p className="text-md">로그인 설명은 여기에 적습니다.</p>
+      <h1 className="text-md font-medium">로그인 폼 제목</h1>
+      <p className="text-md">로그인 폼 설명</p>
     </header>
   );
 }
