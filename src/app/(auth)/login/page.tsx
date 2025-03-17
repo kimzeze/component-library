063a7a0d4302c3a52f7b52a944d36fc431a4485c
kimzeze <fridaynight@kakao.com>
2025-03-17 17:01:07 +0900

    style: 로그인 페이지의 메인 컨테이너에 둥근 모서리 추가
    로그인 페이지의 메인 컨테이너에 rounded-lg 클래스를 추가하여 UI의 시각적 요소를 개선하였습니다.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm, LoginFormFooter, LoginFormHeader } from "../_components";
 export default function LoginPage() {
   return (
     <div className="flex min-h-dvh flex-col items-center justify-center bg-gray-50">
-      <main className="w-[500px] bg-white p-5xl shadow-md">
+      <main className="w-[500px] rounded-lg bg-white p-5xl shadow-md">
         <LoginFormHeader />
         <LoginForm />
         <LoginFormFooter />
